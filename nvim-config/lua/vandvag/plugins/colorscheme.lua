e7944765cd5ec60cbfe9de2def172c3282828114
Evangelos Vandoros <vandvag@gmail.com>
2024-05-05 21:14:53 +0300

    NVIM: Updated colorscheme

--- nvim-config/lua/vandvag/plugins/colorscheme.lua
+++ nvim-config/lua/vandvag/plugins/colorscheme.lua
@@ -1,92 +1,37 @@
 return {
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
 		priority = 1000,
-		config = function()
-			local catppuccin = require("catppuccin")
+		config = function() 
+			local kanagawa = require("kanagawa")
 
-			catppuccin.setup({
-				flavour = "macchiato", -- latte, frappe, macchiato, mocha
-				background = { -- :h background
-					light = "latte",
-					dark = "mocha",
+			kanagawa.setup({
+				compile = false,             -- enable compiling the colorscheme
+				undercurl = true,            -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true},
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = false,         -- do not set background color
+				dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+				colors = {                   -- add/modify theme and palette colors
+						palette = {},
+						theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
 				},
-				transparent_background = false, -- disables setting the background color.
-				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-				term_colors = false,        -- sets terminal colors (e.g. `g:terminal_color_0`)
-				dim_inactive = {
-					enabled = false,          -- dims the background color of inactive window
-					shade = "dark",
-					percentage = 0.15,        -- percentage of the shade to apply to the inactive window
+				overrides = function(colors) -- add/modify highlights
+						return {}
+				end,
+				theme = "wave",              -- Load "wave" theme when 'background' option is not set
+				background = {               -- map the value of 'background' option to a theme
+						dark = "wave",           -- try "dragon" !
+						light = "lotus"
 				},
-				no_italic = true,           -- Force no italic
-				no_bold = false,            -- Force no bold
-				no_underline = true,        -- Force no underline
-				styles = {                  -- Handles the styles of general hi groups (see `:h highlight-args`):
-					-- comments = { "italic" }, -- Change the style of comments
-					conditionals = { "italic" },
-					loops = {},
-					functions = {},
-					keywords = {},
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-					operators = {},
-				},
-				color_overrides = {},
-				custom_highlights = {},
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					telescope = true,
-					notify = false,
-					mini = false,
-					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-				},
-
 			})
 
-			vim.cmd([[colorscheme catppuccin]])
+			vim.cmd([[colorscheme kanagawa]])
 		end
-	},
-	-- {
-	-- 	"rebelot/kanagawa.nvim",
-	-- 	name = "kanagawa",
-	-- 	priority = 1000,
-	-- 	config = function() 
-	-- 		local kanagawa = require("kanagawa")
-
-	-- 		kanagawa.setup({
-	-- 			compile = false,             -- enable compiling the colorscheme
-	-- 			undercurl = true,            -- enable undercurls
-	-- 			commentStyle = { italic = true },
-	-- 			functionStyle = {},
-	-- 			keywordStyle = { italic = true},
-	-- 			statementStyle = { bold = true },
-	-- 			typeStyle = {},
-	-- 			transparent = false,         -- do not set background color
-	-- 			dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-	-- 			terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-	-- 			colors = {                   -- add/modify theme and palette colors
-	-- 					palette = {},
-	-- 					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-	-- 			},
-	-- 			overrides = function(colors) -- add/modify highlights
-	-- 					return {}
-	-- 			end,
-	-- 			theme = "wave",              -- Load "wave" theme when 'background' option is not set
-	-- 			background = {               -- map the value of 'background' option to a theme
-	-- 					dark = "wave",           -- try "dragon" !
-	-- 					light = "lotus"
-	-- 			},
-	-- 		})
-
-	-- 		vim.cmd([[colorscheme kanagawa]])
-	-- 	end
-	-- }
+	}
 }
