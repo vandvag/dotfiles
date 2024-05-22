c194c0687a129699541e6a249d86c5c98064a58b
Evangelos Vandoros <vandvag@gmail.com>
2024-05-22 15:45:02 +0300

    NVIM: Added more mini.nvim plugins

--- nvim-config/lua/vandvag/plugins/mini.lua
+++ nvim-config/lua/vandvag/plugins/mini.lua
@@ -4,6 +4,11 @@ return {
     config = function()
       require("mini.surround").setup()
       require("mini.notify").setup()
+      require("mini.starter").setup()
+      require("mini.indentscope").setup({
+        animation = require("mini.indentscope").gen_animation.none()
+      }
+      )
     end,
   },
 }
