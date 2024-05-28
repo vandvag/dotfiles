10aa5da0f54e144cfa5211878e8f177da4a5c2bf
Evangelos Vandoros <vandvag@gmail.com>
2024-05-28 22:03:06 +0300

    NVIM: Added mini.indentscope

--- nvim-config/lua/vandvag/plugins/mini.lua
+++ nvim-config/lua/vandvag/plugins/mini.lua
@@ -6,6 +6,7 @@ return {
       require("mini.notify").setup()
       require("mini.tabline").setup()
       require("mini.files").setup()
+      require("mini.indentscope").setup()
     end,
   },
 }
