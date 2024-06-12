1f3ddfef2bc09969afbae6acec24e6c22e1fad07
Evangelos Vandoros <vandvag@gmail.com>
2024-06-12 22:06:23 +0300

    NVIM: Updated trouble config

--- nvim-config/lua/vandvag/plugins/trouble.lua
+++ nvim-config/lua/vandvag/plugins/trouble.lua
@@ -1,11 +1,14 @@
 return {
-	"folke/trouble.nvim",
+  "folke/trouble.nvim",
+  cmd = "Trouble",
+  event = "VeryLazy",
+  dependencies = "nvim-tree/nvim-web-devicons",
 
-	config = function()
-		local trouble = require("trouble")
+  config = function()
+    require("trouble").setup {
+    }
 
-		local opts = { noremap = true, silent = true }
-
-		vim.keymap.set("n", "<leader>d", function() trouble.toggle() end, opts)
-	end,
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "<leader>d", "<cmd>Trouble diagnostics toggle<cr>", opts)
+  end,
 }
