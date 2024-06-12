9bbce0fce5797e5d8d302f5d56c9c147e9564850
Evangelos Vandoros <vandvag@gmail.com>
2024-06-12 22:03:56 +0300

    WEZTERM: Updated colorscheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,17 +6,20 @@ local config = {}
 
 config.font = wezterm.font "CaskaydiaCove Nerd Font"
 config.font_size = 14.0
-config.color_scheme = "rose-pine"
+config.color_scheme = "tokyonight_storm"
 config.automatically_reload_config = true
 config.show_tabs_in_tab_bar = true
 config.enable_tab_bar = false
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 config.window_decorations = "RESIZE"
 
 config.window_padding = {
-  left = PADDING,
-  right = PADDING,
-  top = PADDING,
-  botton = PADDING
+	left = PADDING,
+	right = PADDING,
+	top = PADDING,
+	bottom = PADDING
 }
+
 
 return config
