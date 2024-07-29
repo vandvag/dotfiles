ae577268709504a07b90450070c8b8a71ec46b97
Evangelos Vandoros <vandvag@gmail.com>
2024-07-29 21:24:37 +0300

    NVIM: Changes to telescope

--- nvim-config/lua/vandvag/plugins/telescope.lua
+++ nvim-config/lua/vandvag/plugins/telescope.lua
@@ -16,21 +16,28 @@ return {
 				path_display = { "truncate" },
 				mappings = {
 					i = {
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 					},
 					n = {
 						["q"] = actions.close
 					},
 				},
 				file_ignore_patterns = {
-					".git/"
+					".git/",
+					".hg/",
+					"build*/"
 				},
 			},
 			pickers = {
 				colorscheme = {
 					enable_preview = true
+				},
+				find_files = {
+					previewer = true,
+					hidden = true,
+				},
+				lsp_dynamic_workspace_symbols = {
+					theme = "cursor",
+					previewer = false,
 				}
 			}
 		})
@@ -41,14 +48,12 @@ return {
 		-- set keymaps
 		local opts = { noremap = true, silent = true }
 		local keymap = vim.keymap
-		keymap.set("n", "<leader>f",
-			"<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false, hidden = true}))<cr>",
-			opts)
+		keymap.set("n", "<leader>ff", ":Telescope find_files<cr>")
 		keymap.set("n", "<leader>t",
-			"<cmd>lua require'telescope.builtin'.lsp_workspace_symbols(require('telescope.themes').get_dropdown({ previewer = false}))<cr>",
+			"<cmd>lua require'telescope.builtin'.lsp_dynamic_workspace_symbols(require('telescope.themes').get_dropdown({ previewer = false}))<cr>",
 			opts)
 		keymap.set("n", "<leader>r", "<cmd>lua require'telescope.builtin'.live_grep()<cr>", opts)
-		keymap.set("n", "<leader>a", "<cmd>lua require'telescope.builtin'.buffers()<cr>", opts)
+		keymap.set("n", "<leader>ba", "<cmd>lua require'telescope.builtin'.buffers()<cr>", opts)
 		keymap.set("n", "<leader>kt", ":Telescope colorscheme<cr>")
 		keymap.set("n", "<leader>q", ":Telescope marks<cr>")
 		keymap.set("n", "<leader>u", ":Telescope undo<cr>")
