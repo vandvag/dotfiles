7076b9e24983a978714cb8a57393f6aadeda6008
vandvag <vandvag@gmail.com>
2024-11-24 11:10:30 +0200

    NVIM: Updated trouble config

--- nvim-config/lua/vandvag/plugins/trouble.lua
+++ nvim-config/lua/vandvag/plugins/trouble.lua
@@ -3,12 +3,29 @@ return {
   cmd = "Trouble",
   event = "VeryLazy",
   dependencies = "nvim-tree/nvim-web-devicons",
+	keys = {
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+	},
 
   config = function()
     require("trouble").setup {
     }
 
 		local noremap = require("vandvag.core.utils").noremap
-    noremap("n", "<leader>d", "<cmd>Trouble diagnostics toggle<cr>", "Show Trouble diagnostics")
+    -- noremap("n", "<leader>d", "<cmd>Trouble diagnostics toggle<cr>", "Show Trouble diagnostics")
   end,
 }
