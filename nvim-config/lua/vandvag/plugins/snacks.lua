2b35c6c26eb07a1e8a5ad740d470b490d518310c
vandvag <vandvag@gmail.com>
2024-12-17 00:48:32 +0200

    NVIM: Added snacks.nvim

---
+++ nvim-config/lua/vandvag/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+		debug = { enabled = true },
+		dashboard = { enabled = true },
+		git = { enabled = true },
+		lazygit = { enabled = true },
+		zen = { enabled = true },
+	},
+	keys = {
+		{ "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
+		{ "<leader>z", function() Snacks.zen() end, desc = "Toggle zen mode" },
+		{ "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+	},
+}
