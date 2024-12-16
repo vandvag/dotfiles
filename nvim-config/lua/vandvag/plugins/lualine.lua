7804da23295ec3307687d439eed059f3e73c8d15
vandvag <vandvag@gmail.com>
2024-12-17 01:01:07 +0200

    NVIM: Simplified lualine

--- nvim-config/lua/vandvag/plugins/lualine.lua
+++ nvim-config/lua/vandvag/plugins/lualine.lua
@@ -10,8 +10,10 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = 'auto',
-				component_separators = { left = '', right = '' },
-				section_separators = { left = '', right = '' },
+				-- component_separators = { left = '', right = '' },
+				-- section_separators = { left = '', right = '' },
+				component_separators = { left = '', right = '' },
+				section_separators = { left = '', right = '' },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},
