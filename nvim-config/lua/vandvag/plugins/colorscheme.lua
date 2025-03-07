7fd1c9bc4dc7b890707c0be16ec493e8553f5d18
vandvag <vandvag@gmail.com>
2025-03-07 23:29:53 +0200

    NVIM: Added kanagawa theme

--- nvim-config/lua/vandvag/plugins/colorscheme.lua
+++ nvim-config/lua/vandvag/plugins/colorscheme.lua
@@ -1,148 +1,160 @@
 return {
-  -- Kanagawa
-  {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    priority = 1000,
-    config = function()
-      local kanagawa = require("kanagawa")
+	-- Kanagawa
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
+		priority = 1000,
+		config = function()
+			local kanagawa = require("kanagawa")
 
-      kanagawa.setup({
-        compile = false,  -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false,   -- do not set background color
-        dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = {             -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(colors) -- add/modify highlights
-          return {}
-        end,
-        theme = "wave",  -- Load "wave" theme when 'background' option is not set
-        background = {   -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus"
-        },
-      })
-    end
-  },
-  -- Midnight
-  {
-    'dasupradyumna/midnight.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  -- Github
-  {
-    'projekt0n/github-nvim-theme',
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require('github-theme').setup({
-      })
-    end,
-  },
-  -- catppuccin
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
-  },
-  -- rose-pine
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
-    config = function()
-      require("rose-pine").setup({
-        variant = "auto",      -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
+			kanagawa.setup({
+				compile = false, -- enable compiling the colorscheme
+				undercurl = true, -- enable undercurls
+				commentStyle = { italic = true },
+				functionStyle = {},
+				keywordStyle = { italic = true },
+				statementStyle = { bold = true },
+				typeStyle = {},
+				transparent = false, -- do not set background color
+				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+				terminalColors = true, -- define vim.g.terminal_color_{0,17}
+				colors = {         -- add/modify theme and palette colors
+					palette = {},
+					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+				},
+				overrides = function(colors) -- add/modify highlights
+					return {}
+				end,
+				theme = "wave", -- Load "wave" theme when 'background' option is not set
+				background = { -- map the value of 'background' option to a theme
+					dark = "wave", -- try "dragon" !
+					light = "lotus"
+				},
+			})
+		end
+	},
+	-- Midnight
+	{
+		'dasupradyumna/midnight.nvim',
+		lazy = false,
+		priority = 1000,
+	},
+	-- Github
+	{
+		'projekt0n/github-nvim-theme',
+		lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require('github-theme').setup({
+			})
+		end,
+	},
+	-- catppuccin
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000
+	},
+	-- rose-pine
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				variant = "auto",  -- auto, main, moon, or dawn
+				dark_variant = "main", -- main, moon, or dawn
+				dim_inactive_windows = false,
+				extend_background_behind_borders = true,
 
-        enable = {
-          terminal = true,
-          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true,        -- Handle deprecated options automatically
-        },
+				enable = {
+					terminal = true,
+					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+					migrations = true,   -- Handle deprecated options automatically
+				},
 
-        styles = {
-          bold = true,
-          italic = false,
-          transparency = false,
-        },
+				styles = {
+					bold = true,
+					italic = false,
+					transparency = false,
+				},
 
-        groups = {
-          border = "muted",
-          link = "iris",
-          panel = "surface",
+				groups = {
+					border = "muted",
+					link = "iris",
+					panel = "surface",
 
-          error = "love",
-          hint = "iris",
-          info = "foam",
-          note = "pine",
-          todo = "rose",
-          warn = "gold",
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					note = "pine",
+					todo = "rose",
+					warn = "gold",
 
-          git_add = "foam",
-          git_change = "rose",
-          git_delete = "love",
-          git_dirty = "rose",
-          git_ignore = "muted",
-          git_merge = "iris",
-          git_rename = "pine",
-          git_stage = "iris",
-          git_text = "rose",
-          git_untracked = "subtle",
+					git_add = "foam",
+					git_change = "rose",
+					git_delete = "love",
+					git_dirty = "rose",
+					git_ignore = "muted",
+					git_merge = "iris",
+					git_rename = "pine",
+					git_stage = "iris",
+					git_text = "rose",
+					git_untracked = "subtle",
 
-          h1 = "iris",
-          h2 = "foam",
-          h3 = "rose",
-          h4 = "gold",
-          h5 = "pine",
-          h6 = "foam",
-        },
+					h1 = "iris",
+					h2 = "foam",
+					h3 = "rose",
+					h4 = "gold",
+					h5 = "pine",
+					h6 = "foam",
+				},
 
-        highlight_groups = {
-          -- Comment = { fg = "foam" },
-          -- VertSplit = { fg = "muted", bg = "muted" },
-        },
+				highlight_groups = {
+					-- Comment = { fg = "foam" },
+					-- VertSplit = { fg = "muted", bg = "muted" },
+				},
 
-        before_highlight = function(group, highlight, palette)
-          -- Disable all undercurls
-          -- if highlight.undercurl then
-          --     highlight.undercurl = false
-          -- end
-          --
-          -- Change palette colour
-          -- if highlight.fg == palette.pine then
-          --     highlight.fg = palette.foam
-          -- end
-        end,
-      })
-    end
-  },
-  -- Melange
-  { "savq/melange-nvim" },
-  -- Eldritch
-  { "eldritch-theme/eldritch.nvim" },
-  -- Fluoromachine
-  { "maxmx03/fluoromachine.nvim" },
-  { "ricardoraposo/gruvbox-minor.nvim" },
-  { "rktjmp/lush.nvim" },
-  { "craftzdog/solarized-osaka.nvim" },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  { "Mofiqul/vscode.nvim" }
+				before_highlight = function(group, highlight, palette)
+					-- Disable all undercurls
+					-- if highlight.undercurl then
+					--     highlight.undercurl = false
+					-- end
+					--
+					-- Change palette colour
+					-- if highlight.fg == palette.pine then
+					--     highlight.fg = palette.foam
+					-- end
+				end,
+			})
+		end
+	},
+	-- Melange
+	{ "savq/melange-nvim" },
+	-- Eldritch
+	{ "eldritch-theme/eldritch.nvim" },
+	-- Fluoromachine
+	{ "maxmx03/fluoromachine.nvim" },
+	{ "ricardoraposo/gruvbox-minor.nvim" },
+	{ "rktjmp/lush.nvim" },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{ "Mofiqul/vscode.nvim" },
+	{
+		'maxmx03/solarized.nvim',
+		lazy = false,
+		priority = 1000,
+		---@type solarized.config
+		opts = {},
+		config = function(_, opts)
+			vim.o.termguicolors = true
+			vim.o.background = 'light'
+			require('solarized').setup(opts)
+			vim.cmd.colorscheme 'solarized'
+		end,
+	}
 }
