019ff566697851734b6a6153cd2312b54b8e2464
vandvag <vandvag@gmail.com>
2025-03-26 22:39:55 +0200

    NVIM: Removed mini.tabline

--- nvim-config/lua/vandvag/plugins/mini.lua
+++ nvim-config/lua/vandvag/plugins/mini.lua
@@ -5,7 +5,6 @@ return {
 			require("mini.ai").setup()
 			require("mini.surround").setup()
 			require("mini.notify").setup()
-			-- require("mini.tabline").setup()
 			require("mini.files").setup()
 			require("mini.indentscope").setup()
 			require("mini.cursorword").setup()
