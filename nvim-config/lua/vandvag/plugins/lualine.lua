feabc7a92e536a782b52c8cd27b93a75d3d196c6
vandvag <vandvag@gmail.com>
2025-04-09 21:47:18 +0300

    NVIM: Added previous command display on lualine

--- nvim-config/lua/vandvag/plugins/lualine.lua
+++ nvim-config/lua/vandvag/plugins/lualine.lua
@@ -1,17 +1,18 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "echasnovski/mini.icons" },
-	event = { "BufReadPre", "BufNewFile" },
-
+	dependencies = {
+		"echasnovski/mini.icons",
+		"folke/noice.nvim"
+	},
+	event = {
+		"BufReadPre", "BufNewFile"
+	},
 	config = function()
 		local lualine = require("lualine")
-
 		lualine.setup({
 			options = {
 				icons_enabled = true,
 				theme = 'auto',
-				-- component_separators = { left = '', right = '' },
-				-- section_separators = { left = '', right = '' },
 				component_separators = { left = '', right = '' },
 				section_separators = { left = '', right = '' },
 				disabled_filetypes = {
@@ -31,7 +32,15 @@ return {
 				lualine_a = { 'mode' },
 				lualine_b = { 'branch', 'diff', 'diagnostics' },
 				lualine_c = { 'filename' },
-				lualine_x = { 'encoding', 'fileformat', 'filetype' },
+				lualine_x = {
+					{
+						require("noice").api.status.command.get,
+						cond = require("noice").api.status.command.has,
+					},
+					'encoding',
+					'fileformat',
+					'filetype'
+				},
 				lualine_y = { 'progress' },
 				lualine_z = { 'location' }
 			},
