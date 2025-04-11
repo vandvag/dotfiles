9707e5f743419f178464f473d74f18c5c555afc4
vandvag <vandvag@gmail.com>
2025-04-11 21:20:24 +0300

    NVIM: Updated colorschemes.lua
    Changes:
    - Removed solarized.nvim
    - Removed config function for most themes and replaced it with opts
    table

--- nvim-config/lua/vandvag/plugins/colorscheme.lua
+++ nvim-config/lua/vandvag/plugins/colorscheme.lua
@@ -2,61 +2,29 @@ return {
 	-- Ef themes
 	{
 		"oonamo/ef-themes.nvim",
-		config = function()
-			local ef = require("ef-themes")
-			ef.setup({
-				light = "ef-day",
-				dark = "ef-autumn",
-				styles = {
-					comments = { italic = false },
-					pickers = "borderless" -- can be default
-				},
-			})
-		end
+		opts = {
+			light = "ef-day",
+			dark = "ef-bio",
+			styles = {
+				comments = { italic = false },
+				pickers = "borderless" -- can be default
+			},
+		},
 	},
 	-- Kanagawa
 	{
 		"rebelot/kanagawa.nvim",
 		name = "kanagawa",
 		priority = 1000,
-		config = function()
-			local kanagawa = require("kanagawa")
-
-			kanagawa.setup({
-				compile = false, -- enable compiling the colorscheme
-				undercurl = true, -- enable undercurls
-				commentStyle = { italic = true },
-				functionStyle = {},
-				keywordStyle = { italic = true },
-				statementStyle = { bold = true },
-				typeStyle = {},
-				transparent = false, -- do not set background color
-				dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-				terminalColors = true, -- define vim.g.terminal_color_{0,17}
-				colors = {         -- add/modify theme and palette colors
-					palette = {},
-					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-				},
-				overrides = function(colors) -- add/modify highlights
-					return {}
-				end,
-				theme = "wave", -- Load "wave" theme when 'background' option is not set
-				background = { -- map the value of 'background' option to a theme
-					dark = "wave", -- try "dragon" !
-					light = "lotus"
-				},
-			})
-		end
+		opts = {
+			theme = "wave"
+		},
 	},
 	-- Github
 	{
 		'projekt0n/github-nvim-theme',
 		lazy = false,  -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require('github-theme').setup({
-			})
-		end,
 	},
 	-- catppuccin
 	{
@@ -69,74 +37,7 @@ return {
 		"rose-pine/neovim",
 		name = "rose-pine",
 		priority = 1000,
-		config = function()
-			require("rose-pine").setup({
-				variant = "auto",  -- auto, main, moon, or dawn
-				dark_variant = "main", -- main, moon, or dawn
-				dim_inactive_windows = false,
-				extend_background_behind_borders = true,
-
-				enable = {
-					terminal = true,
-					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-					migrations = true,   -- Handle deprecated options automatically
-				},
-
-				styles = {
-					bold = true,
-					italic = false,
-					transparency = false,
-				},
-
-				groups = {
-					border = "muted",
-					link = "iris",
-					panel = "surface",
-
-					error = "love",
-					hint = "iris",
-					info = "foam",
-					note = "pine",
-					todo = "rose",
-					warn = "gold",
-
-					git_add = "foam",
-					git_change = "rose",
-					git_delete = "love",
-					git_dirty = "rose",
-					git_ignore = "muted",
-					git_merge = "iris",
-					git_rename = "pine",
-					git_stage = "iris",
-					git_text = "rose",
-					git_untracked = "subtle",
-
-					h1 = "iris",
-					h2 = "foam",
-					h3 = "rose",
-					h4 = "gold",
-					h5 = "pine",
-					h6 = "foam",
-				},
-
-				highlight_groups = {
-					-- Comment = { fg = "foam" },
-					-- VertSplit = { fg = "muted", bg = "muted" },
-				},
-
-				before_highlight = function(group, highlight, palette)
-					-- Disable all undercurls
-					-- if highlight.undercurl then
-					--     highlight.undercurl = false
-					-- end
-					--
-					-- Change palette colour
-					-- if highlight.fg == palette.pine then
-					--     highlight.fg = palette.foam
-					-- end
-				end,
-			})
-		end
+		opts = {},
 	},
 	-- Melange
 	{ "savq/melange-nvim" },
@@ -153,17 +54,4 @@ return {
 		opts = {},
 	},
 	{ "Mofiqul/vscode.nvim" },
-	{
-		'maxmx03/solarized.nvim',
-		lazy = false,
-		priority = 1000,
-		---@type solarized.config
-		opts = {},
-		config = function(_, opts)
-			vim.o.termguicolors = true
-			vim.o.background = 'light'
-			require('solarized').setup(opts)
-			vim.cmd.colorscheme 'solarized'
-		end,
-	}
 }
