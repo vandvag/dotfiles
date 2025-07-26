0f47ddbfb54065eb89ed0ca847b2e8f018350a07
vandvag <vandvag@gmail.com>
2025-07-26 11:49:55 +0300

    [NVIM]: Updated lsp configuration

--- nvim-config/lua/vandvag/plugins/lsp/lspconfig.lua
+++ nvim-config/lua/vandvag/plugins/lsp/lspconfig.lua
@@ -1,73 +1,62 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		'saghen/blink.cmp',
-		-- "hrsh7th/cmp-nvim-lsp",
-		-- { "antosha417/nvim-lsp-file-operations", config = true },
-	},
-	opts = {
-		servers = {
-			rust_analyzer = {
-				settings = {
-					["rust-analyzer"] = {
-						checkOnSave = {
-							command = "clippy"
-						}
-					}
-				}
-			},
-			clangd = {},
-			pyright = {},
-			zls = {},
-			gopls = {},
-			lua_ls = {
-				settings = { -- custom settings for lua
-					Lua = {
-						-- make the language server recognize "vim" global
-						diagnostics = {
-							globals = { "vim", "Snacks", "MiniFiles" },
-						},
-						workspace = {
-							-- make language server aware of runtime files
-							library = {
-								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-								[vim.fn.stdpath("config") .. "/lua"] = true,
-							},
-						},
-					},
-				},
-			},
-		}
-	},
-	config = function(_, opts)
-		local lspconfig = require("lspconfig")
-		local noremap = require("vandvag.core.utils").noremap
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    'saghen/blink.cmp',
+  },
+  opts = {
+    servers = {
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            checkOnSave = {
+              command = "clippy"
+            }
+          }
+        }
+      },
+      clangd = {},
+      pyright = {},
+      zls = {},
+      gopls = {},
+      lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim", "Snacks", "MiniFiles" },
+            },
+            workspace = {
+              library = {
+                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                [vim.fn.stdpath("config") .. "/lua"] = true,
+              },
+            },
+          },
+        },
+      },
+    }
+  },
 
-		local on_attach = function(_, bufnr)
-			local additional_opts = { buffer = bufnr }
-			noremap('n', 'gD', ":lua vim.lsp.buf.declarations()<cr>", "Go to declaration", additional_opts)
-			noremap('n', 'gd', ":lua vim.lsp.buf.definition()<cr>", "Go to definition", additional_opts)
-			noremap('n', 'gt', "<cmd>Telescope lsp_type_definitions<cr>", "Show type definitions", additional_opts)
-			noremap('n', 'gi', "<cmd>Telescope lsp_implementations<cr>", "Show type implementations", additional_opts)
-			noremap("n", '<leader>e', function() vim.diagnostic.open_float() end, "Show diagnostics for line", additional_opts)
-			local toggle_virtual_lines = function()
-				local new_val = not vim.diagnostic.config().virtual_lines
-				local new_v_text_val = not vim.diagnostic.config().virtual_text
-				vim.diagnostic.config({
-					virtual_lines = new_val, virtual_text = new_v_text_val })
-			end
-			noremap('n', 'gk', toggle_virtual_lines, "Toggle virtual lines", additional_opts)
-			vim.diagnostic.config({
-				virtual_lines = true,
-				virtual_text = false
-			})
-		end
+  config = function(_, opts)
+    local lspconfig = require("lspconfig")
+    local noremap = require("vandvag.core.utils").noremap
+
+    local on_attach = function(_, bufnr)
+      local additional_opts = { buffer = bufnr }
+      noremap('n', 'gD', ":lua vim.lsp.buf.declarations()<cr>", "Go to declaration", additional_opts)
+      noremap('n', 'gd', ":lua vim.lsp.buf.definition()<cr>", "Go to definition", additional_opts)
+      noremap("n", '<leader>e', function() vim.diagnostic.open_float() end, "Show diagnostics for line", additional_opts)
+    end
+
+    vim.diagnostic.config({
+      virtual_lines = false,
+      virtual_text = true
+    })
 
 
-		for server, config in pairs(opts.servers) do
-			config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-			config.on_attach = on_attach
-			lspconfig[server].setup(config)
-		end
-	end
+    for server, config in pairs(opts.servers) do
+      config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      config.on_attach = on_attach
+      lspconfig[server].setup(config)
+    end
+  end
 }
