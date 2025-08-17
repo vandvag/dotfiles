90958e56d22703f909f4f90ca4b922a6408c8d56
vandvag <vandvag@gmail.com>
2025-08-17 13:49:22 +0300

    [NVIM]: Updated keymaps.

--- nvim-config/lua/vandvag/plugins/which_key.lua
+++ nvim-config/lua/vandvag/plugins/which_key.lua
@@ -1,23 +1,26 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		spec = {
-			mode = { "n", "v" },
-			{ "<leader>c", group = "code" },
-			{ "<leader>f", group = "find/file" },
-			{ "<leader>b", group = "buffer" },
-			{ "g",         group = "goto" },
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    spec = {
+      mode = { "n", "v" },
+      { "<leader>c", group = "code" },
+      { "<leader>f", group = "find/file" },
+      { "<leader>b", group = "buffer" },
+      { "<leader>s", group = "search" },
+      { "<leader>q", group = "quickfix" },
+      { "<leader>g", group = "git" },
+      { "g",         group = "goto" },
 
-		},
-	},
-	keys = {
-		{
-			"<leader>?",
-			function()
-				require("which-key").show({ global = false })
-			end,
-			desc = "Buffer Local Keymaps (which-key)",
-		},
-	},
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
 }
