590548da14e9586138bf0211af80b9d4008b3c27
vandvag <vandvag@gmail.com>
2025-08-22 22:01:51 +0300

    [NVIM]: Updates to mini plugin
    - Updated file formatting
    - Now mini is loaded on startup

--- nvim-config/lua/vandvag/plugins/mini.lua
+++ nvim-config/lua/vandvag/plugins/mini.lua
@@ -1,21 +1,20 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    config = function()
-      require("mini.ai").setup()
-      require("mini.surround").setup()
-      require("mini.notify").setup()
-      require("mini.files").setup()
-      require("mini.cursorword").setup()
-      require("mini.icons").setup()
-      require("mini.diff").setup()
-      require("mini.pairs").setup()
-      require("mini.bracketed").setup()
-      require("mini.extra").setup()
-      require("mini.statusline").setup()
-    end,
-    keys = {
-      { "<leader>fm",       function() MiniFiles.open() end, desc = "Open MiniFiles" },
-    }
-  },
+  "echasnovski/mini.nvim",
+  lazy = false,
+  config = function()
+    require("mini.ai").setup()
+    require("mini.surround").setup()
+    require("mini.notify").setup()
+    require("mini.files").setup()
+    require("mini.cursorword").setup()
+    require("mini.icons").setup()
+    require("mini.diff").setup()
+    require("mini.pairs").setup()
+    require("mini.bracketed").setup()
+    require("mini.extra").setup()
+    require("mini.statusline").setup()
+  end,
+  keys = {
+    { "<leader>fm", function() MiniFiles.open() end, desc = "Open MiniFiles" },
+  }
 }
