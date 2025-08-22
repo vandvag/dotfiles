1d2f6e85545c2514ca84cec4604f680be38d111e
vandvag <vandvag@gmail.com>
2025-08-22 22:54:38 +0300

    [NVIM]: Changed harpoon keys

--- nvim-config/lua/vandvag/plugins/harpoon.lua
+++ nvim-config/lua/vandvag/plugins/harpoon.lua
@@ -5,25 +5,22 @@ return {
   },
   keys = {
     {
-      "<leader>hs", ":Telescope harpoon marks<cr>", desc = "Show harpoon marks"
+      "<leader>jk", ":lua require('harpoon.mark').add_file()<cr>", desc = "Add harpoon mark"
     },
     {
-      "<leader>hm", ":lua require('harpoon.mark').add_file()<cr>", desc = "Add harpoon mark"
+      "<leader>jj", ":lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Toggle harpoon quick menu"
     },
     {
-      "<leader>hh", ":lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Toggle harpoon quick menu"
+      "<leader>1", ":lua require('harpoon.ui').nav_file(1)<cr>", desc = "Go to harpoon mark 1"
     },
     {
-      "<a-1>", ":lua require('harpoon.ui').nav_file(1)<cr>", desc = "Go to harpoon mark 1"
+      "<leader>2", ":lua require('harpoon.ui').nav_file(2)<cr>", desc = "Go to harpoon mark 2"
     },
     {
-      "<a-2>", ":lua require('harpoon.ui').nav_file(2)<cr>", desc = "Go to harpoon mark 2"
+      "<leader>3", ":lua require('harpoon.ui').nav_file(3)<cr>", desc = "Go to harpoon mark 3"
     },
     {
-      "<a-3>", ":lua require('harpoon.ui').nav_file(3)<cr>", desc = "Go to harpoon mark 3"
-    },
-    {
-      "<a-4>", ":lua require('harpoon.ui').nav_file(4)<cr>", desc = "Go to harpoon mark 4"
+      "<leader>4", ":lua require('harpoon.ui').nav_file(4)<cr>", desc = "Go to harpoon mark 4"
     },
   },
 }
