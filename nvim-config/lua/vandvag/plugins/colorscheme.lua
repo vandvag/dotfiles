6d568bae2a3c00d3cd52828025589e2948133463
vandvag <vandvag@gmail.com>
2025-08-22 21:57:37 +0300

    [NVIM]: Removed several colorschemes

--- nvim-config/lua/vandvag/plugins/colorscheme.lua
+++ nvim-config/lua/vandvag/plugins/colorscheme.lua
@@ -20,18 +20,6 @@ return {
       theme = "wave"
     },
   },
-  -- Github
-  {
-    'projekt0n/github-nvim-theme',
-    lazy = false,  -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-  },
-  -- catppuccin
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
-  },
   -- rose-pine
   {
     "rose-pine/neovim",
@@ -39,20 +27,9 @@ return {
     priority = 1000,
     opts = {},
   },
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  -- Melange
-  { "savq/melange-nvim" },
   -- Eldritch
   { "eldritch-theme/eldritch.nvim" },
-  -- Fluoromachine
-  { "maxmx03/fluoromachine.nvim" },
   { "ricardoraposo/gruvbox-minor.nvim" },
-  { "rktjmp/lush.nvim" },
   {
     "folke/tokyonight.nvim",
     lazy = false,
