7e43e8c118dd6e056b0e95a3466160f6b5878cd7
Evangelos Vandoros <vandvag@gmail.com>
2025-09-01 18:39:37 +0300

    [NVIM]: Added shortcut for current line blame

--- nvim-config/lua/vandvag/plugins/gitsigns.lua
+++ nvim-config/lua/vandvag/plugins/gitsigns.lua
@@ -4,6 +4,7 @@ return {
   config = true,
   keys = {
     { "<leader>gf", "<cmd>Gitsigns blame<cr>",                     desc = "Blame File" },
-    { "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle current line blame" }
+    { "<leader>gl", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle current line blame" },
+    { "<leader>gb", "<cmd>Gitsigns blame_line<cr>",                desc = "Blame current line" }
   }
 }
