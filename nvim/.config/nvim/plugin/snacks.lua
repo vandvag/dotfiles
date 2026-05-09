ca036282af9973415fed7cde3a69479a60b295af
Evangelos Vandoros <vandvag@gmail.com>
2026-05-10 00:01:37 +0300

    [NVIM]: Only kept bigfile from snacks.lua

--- nvim/.config/nvim/plugin/snacks.lua
+++ nvim/.config/nvim/plugin/snacks.lua
@@ -2,14 +2,7 @@ local noremap = require("utils").noremap
 vim.pack.add({ "https://github.com/folke/snacks.nvim" })
 
 require("snacks").setup({
-  notifier = { enabled = true },
-  quickfile = { enabled = true },
   bigfile = { enabled = true },
-  words = { enabled = true },
-  debug = { enabled = true },
-  lazygit = { enabled = true },
-  input = { enabled = true },
 })
 
 noremap("n", "<leader>e", "<Nop>", "Disable (LSP diagnostic handles <leader>e)")
-noremap("n", "<leader>gg", function() Snacks.lazygit() end, "Lazygit")
